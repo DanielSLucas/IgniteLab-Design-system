7d463e4de4877380246f4d8face49477e428c9a2
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-10-12 11:44:15 -0300

    feat: Heading component

---
+++ src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading, HeadingProps } from '.';
+
+export default {
+  title: 'Components/Heading',
+  component: Heading,
+  args: {
+    childen: 'Lorem ipsum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<HeadingProps>;
+
+export const Default: StoryObj<HeadingProps> = {};
+
+export const Small: StoryObj<HeadingProps> = {
+  args: {
+    size: 'sm'
+  }
+};
+
+export const Large: StoryObj<HeadingProps> = {
+  args: {
+    size: 'lg'
+  }
+};
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+  args: {
+    asChild: true,
+    childen: (
+      <h1>Heading with h1</h1>
+    )
+  },
+  argTypes: {
+    childen: {
+      table: {
+        disable: true,
+      }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
+    },
+  }
+};
